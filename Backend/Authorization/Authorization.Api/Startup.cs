0ca3a272a1f54c97c1ca1a93471704a869bf270b
Ruslan Kazankov <kazankovruslankrk@gmail.com>
2024-11-14 16:23:51 +0800

    Add startup pattern

---
+++ Backend/Authorization/Authorization.Api/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Options;
+using System.Runtime;
+
+namespace Authorization.Api;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddSwaggerGen();
+        services.AddEndpointsApiExplorer();
+    }
+
+    public void Configure(IApplicationBuilder app, IHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+            endpoints.MapControllers()
+        );
+
+    }
+}
