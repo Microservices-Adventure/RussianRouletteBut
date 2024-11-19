a5b2a89e1a4e3b6dbf65a475cee019024e0b278f
Ruslan Kazankov <kazankovruslankrk@gmail.com>
2024-11-19 20:53:17 +0800

    Add IServiceCollection extensions

---
+++ Backend/Authorization/Authorization.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,110 @@
+﻿using Authorization.Api.BackgroundServices;
+using Authorization.Api.Config;
+using Authorization.Domain.Config;
+using Authorization.Domain.Entities;
+using Authorization.Domain.Services.Interfaces;
+using Authorization.Domain.Services;
+using Authorization.Domain.Validators;
+using Authorization.Infrastructure.Persistence;
+using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace Authorization.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddAuthorizationServices(this IServiceCollection services, JwtSettings settings)
+    {
+        services
+            .AddAuthorization()
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = settings.Issuer,
+                    ValidateAudience = true,
+                    ValidAudience = settings.Audience,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(settings.SecretKey)),
+                    ValidateIssuerSigningKey = true,
+                };
+            });
+
+        return services;
+    }
+
+    public static IServiceCollection AddDbServices(this IServiceCollection services, string connectionString)
+    {
+        services
+            .AddIdentity<User, IdentityRole<long>>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+            })
+            .AddEntityFrameworkStores<AppDbContext>();
+
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddConfiguires(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSwaggerGen(option =>
+        {
+            option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+            option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+        });
+        services.AddEndpointsApiExplorer();
+
+        services.Configure<JwtSettings>(configuration.GetSection(nameof(KafkaSettings)));
+        services.Configure<JwtSettings>(configuration.GetSection(nameof(JwtSettings)));
+
+        return services;
+    }
+
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssemblyContaining<RegisterUserModelValidator>();
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IAccountService, AccountService>();
+        services.AddHostedService<RegisterBackgroundService>();
+
+        return services;
+    }
+}
--- Backend/Authorization/Authorization.Api/Startup.cs
+++ Backend/Authorization/Authorization.Api/Startup.cs
@@ -25,61 +25,12 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
-        services.AddSwaggerGen(option =>
-        {
-            option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
-            option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-            {
-                In = ParameterLocation.Header,
-                Description = "Please enter a valid token",
-                Name = "Authorization",
-                Type = SecuritySchemeType.Http,
-                BearerFormat = "JWT",
-                Scheme = "Bearer"
-            });
-            option.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        Array.Empty<string>()
-                    }
-                });
-        });
-        services.AddEndpointsApiExplorer();
-
+        services.AddConfiguires(_configuration);
         services.AddAuthorizationServices(_configuration.GetSection(nameof(JwtSettings)).Get<JwtSettings>()!);
 
-        services.Configure<JwtSettings>(_configuration.GetSection(nameof(KafkaSettings)));
-        services.Configure<JwtSettings>(_configuration.GetSection(nameof(JwtSettings)));
-
-        services
-            .AddIdentity<User, IdentityRole<long>>(options =>
-            {
-                options.Password.RequireDigit = true;
-                options.Password.RequiredLength = 8;
-                options.Password.RequireLowercase = true;
-                options.Password.RequireUppercase = true;
-            })
-            .AddEntityFrameworkStores<AppDbContext>();
-
         var connectionString = _configuration["ConnectionStrings:DefaultConnection"]!;
-        services.AddDbContext<AppDbContext>(options =>
-        {
-            options.UseNpgsql(connectionString);
-        });
-
-        services.AddValidatorsFromAssemblyContaining<RegisterUserModelValidator>();
-
-        services.AddScoped<ITokenService, TokenService>();
-        services.AddScoped<IAccountService, AccountService>();
-        services.AddHostedService<RegisterBackgroundService>();
+        services.AddDbServices(connectionString);
+        services.AddApplicationServices();
     }
 
     public void Configure(IApplicationBuilder app, IHostEnvironment env)
