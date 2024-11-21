f4e27fcb87a22d0c62ad6fa44cd9834e518accaf
Ruslan Kazankov <kazankovruslankrk@gmail.com>
2024-11-21 22:49:31 +0800

    Add pages Revolver, RegisterSend, Login, Register, RegisterSend. Add AccountController.

---
+++ Frontend/Entities/Account/Model/RegisterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Frontend.Entities.Account.Model;
+
+public class RegisterModel
+{
+    public string Username { get; init; }
+    public string Email { get; init; }
+    public string Password { get; init; }
+    public string ConfigrmPassword { get; init; }
+}
---
+++ Frontend/Pages/Account/Model/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Frontend.Entities.Account.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Frontend.Pages.Account.Model;
+
+public class AccountController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(LoginModel loginModel)
+    {
+        return RedirectToAction("Index", "Home");
+    }
+
+    [HttpPost]
+    public IActionResult Register(RegisterModel registerModel)
+    {
+
+        return RedirectToAction("RegisterSend", "Account");
+    }
+
+    public IActionResult RegisterSend(RegisterModel registerModel)
+    {
+
+        return View();
+    }
+}
