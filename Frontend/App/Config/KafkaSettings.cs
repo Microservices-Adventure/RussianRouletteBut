02e33b2ef7b0d042127832afa34b9ae3f3504837
RuslanKazankov <kazankovruslankrk@gmail.com>
2024-11-29 16:52:21 +0800

    Auth-service use only http. Auth-service and client update for docker-compose

--- Frontend/App/Config/KafkaSettings.cs
+++ Frontend/App/Config/KafkaSettings.cs
@@ -2,6 +2,10 @@ namespace Frontend.App.Config;
 
 public class KafkaSettings
 {
-    public string BootstrapServers { get; set; } = null!;
+    private string _bootstrapServer = null!;
+    public string BootstrapServers { 
+        get => Environment.GetEnvironmentVariable("ASPNETCORE_Kafka_Bootstrap_Servers") ?? _bootstrapServer; 
+        set => _bootstrapServer = value; 
+    }
     public string RegistrationTopic { get; set; } = null!;
 }
--- Frontend/App/Kafka/RegisterProducer.cs
+++ Frontend/App/Kafka/RegisterProducer.cs
@@ -19,6 +19,8 @@ public class RegisterProducer : IDisposable
         _logger = producerLogger;
         var builder = new ProducerBuilder<long, string>(new ProducerConfig{
                 BootstrapServers = bootstrapServers,
+                BatchSize = 50000,
+                LingerMs = 5
             });
 
         _producer = builder.Build();
