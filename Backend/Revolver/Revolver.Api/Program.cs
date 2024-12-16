d27bf0a3b8587b7b108d332c2551768624c2ba0a
RuslanKazankov <kazankovruslankrk@gmail.com>
2024-12-16 18:25:54 +0800

    Microfixes

--- Backend/Revolver/Revolver.Api/Program.cs
+++ Backend/Revolver/Revolver.Api/Program.cs
@@ -5,20 +5,6 @@ public class Program
     public static void Main(string[] args)
     {
         CreateHostBuilder(args).Build().Run();
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-
-        app.Run();
     }
 
     private static IHostBuilder CreateHostBuilder(string[] args)
