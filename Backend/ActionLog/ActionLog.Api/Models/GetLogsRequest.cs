b1d2fbad624f5ad34207e63ee4fd8401b705f23f
alljay3 <mihaspiva@yandex.ru>
2025-01-04 12:18:21 +0800

    remove Status

---
+++ Backend/ActionLog/ActionLog.Api/Models/GetLogsRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace ActionLog.Api.Models
+{
+    public class GetLogsRequest
+    {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public long MicroserviceId { get; set; }
+        public string MicroserviceName { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Status { get; set; } = null!;
+        public string? Error { get; set; }
+        public DateTimeOffset Moment { get; set; }
+    }
+}
