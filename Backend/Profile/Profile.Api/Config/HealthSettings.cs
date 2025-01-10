e5828c1d58e32748e93f0b22138c1ff7d840c636
alljay3 <mihaspiva@yandex.ru>
2025-01-10 16:54:19 +0800

    add health

---
+++ Backend/Profile/Profile.Api/Config/HealthSettings.cs
@@ -0,0 +1,25 @@
+﻿namespace Profile.Api.Config
+{
+    public class HealthSettings
+    {
+        public static double CrashTime
+        {
+            get
+            {
+                var crashTime = Environment.GetEnvironmentVariable("HealthSettings_CrashTime")!;
+                return double.Parse(crashTime);
+            }
+        }
+
+        public static double CooldownTime
+        {
+            get
+            {
+                var cooldownTime = Environment.GetEnvironmentVariable("HealthSettings_CooldownTime")!;
+                return double.Parse(cooldownTime);
+            }
+        }
+
+        public static readonly DateTimeOffset AppStartAt = DateTimeOffset.UtcNow;
+    }
+}
