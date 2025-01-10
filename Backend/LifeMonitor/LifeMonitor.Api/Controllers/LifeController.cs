edf5fe8325d7aff99b69807aaa2cfc1fbfa35fa7
alljay3 <mihaspiva@yandex.ru>
2025-01-10 18:21:05 +0800

    create service and controllers

---
+++ Backend/LifeMonitor/LifeMonitor.Api/Controllers/LifeController.cs
@@ -0,0 +1,24 @@
+﻿using LifeMonitor.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifeMonitor.Api.Controllers
+{
+    [ApiController]
+    [Route("api/life")]
+    public class LifeController : ControllerBase
+    {
+        private static Dictionary<string, string> ServiceHostsPorts = new()
+        {
+            { "localhost", "8082" }, // Authorization
+            { "localhost", "8084" }, // Revolver
+            { "localhost", "8086" }, // ActionLog
+            { "localhost", "8088" } // Profile
+        };
+        private readonly IMonitorService _monitorService;
+        public LifeController(IMonitorService monitorService)
+        {
+            _monitorService = monitorService;
+            
+        }
+    }
+}
---
+++ Backend/LifeMonitor/LifeMonitor.Api/Services/MonitorService.cs
@@ -0,0 +1,62 @@
+﻿using LifeMonitor.Api.Models;
+using System.ComponentModel;
+using System.Text.Json;
+
+namespace LifeMonitor.Api.Services
+{
+    public class MonitorService : IMonitorService
+    {
+        public async Task<LifeServiceModel> GetLife(string serviceHost, string port)
+        {
+            var httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) => true;
+            using var httpClient = new HttpClient(httpClientHandler);
+            HttpResponseMessage response = await httpClient.GetAsync("http://" + serviceHost + ":" + port + "/api/hearth/isLive");
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                bool isLive = JsonSerializer.Deserialize<bool>(jsonResponse);
+                LifeServiceModel lifeService = new LifeServiceModel()
+                {
+                    IsLife = isLive,
+                    ServiceName = serviceHost
+                };
+
+                if (!isLive)
+                {
+                    return lifeService;
+                }
+
+                HttpResponseMessage responseKill = await httpClient.GetAsync("http://" + serviceHost + ":" + port + "/api/hearth/killAvailable");
+
+                if (responseKill.IsSuccessStatusCode)
+                {
+                    CooldownModel jsonResponseCooldown = (await response.Content.ReadFromJsonAsync<CooldownModel>())!;
+                    return lifeService with { Cooldown = jsonResponseCooldown };
+
+                }
+
+
+                return new LifeServiceModel()
+                {
+                    IsLife = false,
+                    ServiceName = serviceHost
+                };
+
+
+
+            }
+
+            return new LifeServiceModel()
+            {
+                IsLife = false,
+                ServiceName = serviceHost
+            };
+        }
+
+
+    }
+
+    
+
+}
